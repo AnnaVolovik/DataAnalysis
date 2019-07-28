140e1c600a060e0ca2d37c61489419f07fd0b638
AnnaVolovik <anna_volovik@hotmail.com>
2019-07-28 11:32:18 +0300

    Update assignment_three.py

--- assignment_three.py
+++ assignment_three.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 class DataAnalysis:
 
-    """Demonstrating the ability to acquire, manipluate, clean and run basic data analysis with pandas.
+    """Demonstrating ability to acquire, manipulate, clean and run basic data analysis with pandas.
     Assignment 3 within "Introduction to Data Science in Python" Coursera course by University of Michigan"
     """
 
